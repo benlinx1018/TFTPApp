782ce534bf24278b47bcef922e71e02a533152b4
Lin <good821018@gmail.com>
2016-05-10 05:49:13 +0800

    初版TFTP

--- app/src/main/java/com/tftp/tftpapp/SNMPActivity.java
+++ app/src/main/java/com/tftp/tftpapp/SNMPActivity.java
@@ -38,8 +38,8 @@ public class SNMPActivity extends AppCompatActivity {
     public static final String READ_COMMUNITY = "public";
     public static final String WRITE_COMMUNITY = "private";
     private String tftpFileName = "Unicorn.5511mp1.CALA-D3.PC15.1609.1-9.36.2012.cpr";
-    private String IP;
-    private String tftpIP="192.168.100.1";
+    private String IP="192.168.100.1";
+    private String tftpIP;
     private int tftpPort;
     private ProgressBar mSpinner;
     private TextView txtLog;
